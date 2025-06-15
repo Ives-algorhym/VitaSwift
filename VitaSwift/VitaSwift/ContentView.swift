ce5366dc1d156400d0c95b5374287695e0ba911b
ives-algorhym <algoives@icloud.com>
2025-06-15 13:57:25 -0700

    Initial app

---
+++ VitaSwift/VitaSwift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VitaSwift
+//
+//  Created by Ives Murillo on 6/15/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("VitaSwift")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VitaSwift/VitaSwift/VitaSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitaSwiftApp.swift
+//  VitaSwift
+//
+//  Created by Ives Murillo on 6/15/25.
+//
+
+import SwiftUI
+
+@main
+struct VitaSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
